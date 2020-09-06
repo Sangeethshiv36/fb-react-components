d93abedcdacd3b3896d365ebf49f0c3ce05fec01
Sangeeth Sivan <b.sangeethsivan@gmail.com>
2020-09-07 04:34:06 +0530

    navbar navitem svg icons added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import NavItem from './components/NavItem';
+import { ReactComponent as BellIcon } from './icons/bell.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
 
 function App() {
   return (
     <>
       <Navbar>
-        <NavItem icon="🥑" />
-        <NavItem icon="😁" />
-        <NavItem icon="😂" />
+        <NavItem icon={<PlusIcon />} />
+        <NavItem icon={<BellIcon />} />
+        <NavItem icon={<MessengerIcon />} />
       </Navbar>
     </>
   );
